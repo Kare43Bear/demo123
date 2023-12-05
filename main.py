1dad24e1539a22352ff3400f72f099160a0d852d
kare43Bear <kari.stefanidis@dsbn.org>
2023-12-05 18:14:28 -0500

    final

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('This is a program')
     print("Thank you!")
+    print("final changes")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
